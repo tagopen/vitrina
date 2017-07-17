a2b93a2985150e78ac9b44c9fb6c562de0f3ea80
Security2431 <Artem2431@gmail.com>
2017-07-17 13:19:55 +0300

    update template

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,7 @@
   "use strict"; // Start of use strict
 
   // Old browser notification
-  $(function() {
+  $(function () {
     $.reject({
       reject: {
         msie: 9
@@ -21,7 +21,24 @@
     });
   });
 
-  $(function() {
+  // Bootstrap tooltip
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+  });
+
+  // Selectize
+  $(function () {
+    $(".disperse__select--desirable").select2({
+      maximumInputLength: 2
+    });
+    $(".disperse__select--undesirable").select2({
+      theme: "red"
+    });
+    $(".disperse__select--area, .disperse__select--parking").select2();
+  });
+
+  // Video preview
+  $(function () {
     $('#video__play').on('click', function(e) {
       let dataYoutube = $(this).closest('.video').data('youtube');
       $(this).replaceWith('<div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="https://www.youtube.com/embed/' + dataYoutube + '?autoplay=1" frameborder="0" allowfullscreen></iframe></div>');
@@ -30,12 +47,13 @@
   });
   
   // Tabs panel
-  $(function() {
+  $(function () {
     $('.tab-control__link').on('click', function(e) {
       e.preventDefault();
       var $item = $(this).closest('.tab-control__item'),
           $contentItem = $('.tab__panel'),
           itemPostion = $(this).attr('href');
+
 
           $contentItem.filter(itemPostion)
                       .addClass('tab__panel--active')
@@ -49,85 +67,83 @@
                  .siblings()
                  .find('.tab-control__link')
                  .removeClass('tab-control__link--active');
+      $contentItem.resize();
     });
   });
 
-  // Header slick slider
-  if ($('#carousel-header').length) { 
-    $('#carousel-header').slick({
-      dots: true,
-      dotsClass: "slick-dots slick-dots--inverse",
-      arrows: false,
-      infinite: true,
-      slidesToShow: 1,
-      speed: 500,
-      mobileFirst: true,
-      swipeToSlide: '15',
-      responsive: [
-        {
-          breakpoint: 767,
-          settings: "unslick",
-        }
-      ]
-    });
-  }
-  // Service slick slider
-  if ($('#tab-panel--service1').length) {
-    $('#tab-panel--service1').slick({
-      dotsClass: "slick-dots slick-dots--service",
-      dots: true,
-      arrows: false,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      speed: 500,
-      mobileFirst: true,
-      swipeToSlide: '15',
-      responsive: [
-        {
-          breakpoint: 767,
-          settings: "unslick",
-        }
-      ]
-    });
-  }
+  // FlexSlider setup using custom selector
+  $("#carousel-shop").flexslider({
+    selector: ".shop__item",
+    animation: "slide",
+    animationLoop: false,
+    itemWidth: 210,
+    itemMargin: 5,
+    minItems: 2,
+    maxItems: 4
+  });
 
-  // Service slick slider
-  if ($('#tab-panel--service2').length) {
-    $('#tab-panel--service2').slick({
-      dotsClass: "slick-dots slick-dots--service",
-      dots: true,
-      arrows: false,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      speed: 500,
-      mobileFirst: true,
-      swipeToSlide: '15',
-      responsive: [
-        {
-          breakpoint: 767,
-          settings: "unslick",
-        }
-      ]
-    });
-  }
+  // custom dropdown
+  $(function () {
+    $('.slidedown').on('click', function(e) {
+      e.preventDefault();
+      
+      var $item = $(this),
+          $content = $item.siblings('.faq__answer');
 
-  // Shop slick slider
-  if ($('#carousel-shop').length) { 
-    $('#carousel-shop').slick({
-      dots: true,
-      arrows: false,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      speed: 500,
-      mobileFirst: true,
-      swipeToSlide: '15',
-      responsive: [
-        {
-          breakpoint: 767,
-          settings: "unslick",
-        }
-      ]
+      $item.addClass('faq__question--active')
+      .siblings()
+      .removeClass('faq__question--active');
     });
-  }
+  });
+  
+  $(function () {
+    $('#list-view').click(function( e ) {
+      $('.category__layout').attr('class', 'category__layout category__layout--list col col-lg-12');
+
+      $('.view').removeClass('view--active');
+      $(this).addClass('view--active');
+
+      localStorage.setItem('display', 'list');
+
+      e.preventDefault();
+    });
+
+    $('#map-view').click(function( e ) {
+
+      $('.view').removeClass('view--active');
+      $(this).addClass('view--active');
+
+      localStorage.setItem('display', 'map');
+
+      e.preventDefault();
+    });
+
+    $('#grid-view').click(function( e ) {
+      var $cols = $('.category__layout');
+      $cols.each(function(i, item) {
+        if (i < 3) {
+          $(this).attr('class', 'category__layout category__layout--grid col col-md-8');
+        } else {
+          $(this).attr('class', 'category__layout category__layout--grid col col-md-8 col-lg-6');
+        }
+
+      });
+
+      $('.view').removeClass('view--active');
+      $(this).addClass('view--active');
+      localStorage.setItem('display', 'grid');
+
+      e.preventDefault();
+    });
+
+    if (localStorage.getItem('display') == 'list') {
+      $('#list-view').trigger('click');
+    } else if (localStorage.getItem('display') == 'map') {
+      $('#map-view').trigger('click');
+    } else {
+      $('#grid-view').trigger('click');
+    }
+  });
+
 
 })(jQuery); // End of use strict
