19bab53ca15ec5078b1ef1968ab65044e95be286
Security2431 <Artem2431@gmail.com>
2017-07-18 13:14:26 +0300

    category section

--- app/js/main.js
+++ app/js/main.js
@@ -70,16 +70,68 @@
       $contentItem.resize();
     });
   });
+  
+  $(function () {
+    var totalProducts = (function() {
+      $('.show-all__visible').text($('.category__layout').has(':visible').length);
+      $('.show-all__all').text($('.category__layout').length);
+    });
 
-  // FlexSlider setup using custom selector
-  $("#carousel-shop").flexslider({
-    selector: ".shop__item",
-    animation: "slide",
-    animationLoop: false,
-    itemWidth: 210,
-    itemMargin: 5,
-    minItems: 2,
-    maxItems: 4
+    // show category item
+    $(window).on('resize', function(){
+      var $item = $('.category__layout'),
+          delayStep = 200,
+          showPerClick = 4,
+          totalSize = $item.length;
+
+
+      // hide elements on resize
+      if ($('.show-all__link--catalog').is(':visible')) {
+        $item.hide();
+        if( $( window ).width() <= 767 ) {
+          $('.category__layout:lt(4)').show();
+        } else if( $( window ).width() <= 991 ) {
+         $('.category__layout:lt(6)').show();
+        } else {
+          $('.category__layout:lt(7)').show();
+        }
+      }
+
+      totalProducts();
+
+      // Show hidden elements with fadein animation
+      $(document).on("click", ".show-all__link--catalog", (function(e) {
+        var visibleItems = $item.has(':visible').length,
+            openItems    = (visibleItems + showPerClick <= totalSize) ? visibleItems + showPerClick : totalSize;
+
+        $('.category__layout:lt(' + openItems + ')').each(function(i, item) {
+          $(this).stop().fadeIn('slow').animate({opacity: 1}, delayStep)
+        });
+
+        totalProducts();
+        if (visibleItems == totalSize)
+          $('.show-all__link--catalog').hide();
+
+        e.preventDefault();
+      }));
+
+    }).triggerHandler('resize');
+  });
+
+  // favorite active
+  $(function() {
+    $('.favorite').on('click', function(e) {
+      $(this).toggleClass('favorite--active');
+      e.preventDefault();
+    });
+  });
+
+  // compare active
+  $(function() {
+    $('.compare').on('click', function(e) {
+      $(this).toggleClass('compare--active');
+      e.preventDefault();
+    });
   });
 
   // custom dropdown
@@ -98,7 +150,10 @@
   
   $(function () {
     $('#list-view').click(function( e ) {
-      $('.category__layout').attr('class', 'category__layout category__layout--list col col-lg-12');
+
+      $('.category__col').attr('class', 'col category__col');
+      $('.map--category').addClass('hidden-xs-up');
+      $('.category__layout').attr('class', 'category__layout category__layout--list col col-24 col-lg-12');
 
       $('.view').removeClass('view--active');
       $(this).addClass('view--active');
@@ -109,6 +164,10 @@
     });
 
     $('#map-view').click(function( e ) {
+      $('.map--category').removeClass('hidden-xs-up');
+      $('.category__col').attr('class', 'col col-24 col-md-10 col-lg-8 category__col category__col--map');
+
+      $('.category__layout').attr('class', 'category__layout category__layout--map col col-24 col-md-24');
 
       $('.view').removeClass('view--active');
       $(this).addClass('view--active');
@@ -122,12 +181,15 @@
       var $cols = $('.category__layout');
       $cols.each(function(i, item) {
         if (i < 3) {
-          $(this).attr('class', 'category__layout category__layout--grid col col-md-8');
+          $(this).attr('class', 'category__layout category__layout--grid category__layout--big col-24 col-sm-16 col-md-8');
         } else {
-          $(this).attr('class', 'category__layout category__layout--grid col col-md-8 col-lg-6');
+          $(this).attr('class', 'category__layout category__layout--grid col-24 col-sm-16 col-md-8 col-lg-6');
         }
 
       });
+
+      $('.category__col').attr('class', 'col category__col');
+      $('.map--category').addClass('hidden-xs-up');
 
       $('.view').removeClass('view--active');
       $(this).addClass('view--active');
